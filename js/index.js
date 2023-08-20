151e235fb3042f0a5e83c6cc87e105c0fc8e0044
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-08-20 15:42:33 +0600

    added coupon section and total price section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('aycho go');
+let total = 0;
+function handleClick(target) {
+    //  console.log();
+    let selectedItemContainer = document.getElementById('selected-item');
+    let count = selectedItemContainer.childElementCount;
+    let itemName = target.childNodes[3].childNodes[3].innerText;
+    let p = document.createElement('p');
+    p.innerHTML = `${count + 1}. ${itemName}`;
+    selectedItemContainer.appendChild(p);
+    
+    let itemTotalPrice = target.childNodes[3].childNodes[5].innerText.split(' ')[0];
+    total = parseInt(total) + parseInt(itemTotalPrice) + '.00TK';
+
+    // console.log(total);
+    const totalPrice = document.getElementById('total-price').innerText = total;
+   
+}
+
+
+
+
