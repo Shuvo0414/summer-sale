d49095f15bd960b86c533b5f884e5e384df005a2
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-08-21 08:18:13 +0600

    added discount amount area vale and and added all over price total after discount

--- js/index.js
+++ js/index.js
@@ -34,41 +34,29 @@ function handleClick(target) {
 
 return totalPrice;
 }
-document.getElementById('apply-btn').addEventListener('click',function(){
-   let inputField = document.getElementById('coupon-field').value;
-   if (inputField === 'SELL200'){
-    const discountedAummont = parseInt(total) * 0.20;
-    const fiexdDiscountAmount = discountedAummont.toFixed(2);
-    const discountTotal = parseFloat(total) - fiexdDiscountAmount;
-    console.log(fiexdDiscountAmount, discountTotal);
-   }
+
+function applyBtn (){
     
-})
-    // coupon field.
+        let inputField = document.getElementById('coupon-field').value;
+        if (inputField === 'SELL200'){
+         const discountedAummont = parseInt(total) * 0.20;
+         const fiexdDiscountAmount = discountedAummont.toFixed(2);
+         const discountTotal = parseFloat(total) - fiexdDiscountAmount;
+         console.log(fiexdDiscountAmount, discountTotal);
 
+         const discountAmountArea = document.getElementById('discount-amount');
+         discountAmountArea.innerText = fiexdDiscountAmount + 'TK';
 
+         const discountTotalArea = document.getElementById('discount-total');
+         discountTotalArea.innerText = discountTotal + '0 TK'
 
-
-// function applyCoupon() {
-//     const couponCodeInput = document.getElementById('coupon-field');
-//     const couponCode = couponCodeInput.value;
-
-//     if (couponCode === 'SELL200') {
-//         // Apply a 20% discount
-//         const discountedAummont = totalPrice * 0.20;
-//         const discountedPrice = totalPrice - discountedAummont;
-
-//         console.log(discountedAummont, discountedPrice);
-//         couponCodeInput.value = ''; // Clear the coupon code input
+        }
         
-//       } 
-//    return discountedAummont, discountedPrice;   
-// }
+}
 
-
-// Go home button.
 document.getElementById('go-home-btn').addEventListener('click', function(){
     location.reload();
+    
 })
 
 
